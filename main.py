effde0fed0f214de2c487a8b905391675aa5573c
AndersonLemes2113 <anderson.lemes2113@gmail.com>
2024-03-16 10:27:32 -0300

    Alterações do texto tema e inclementação da funcionalidade de buscar texto do arquivo .txt.

--- main.py
+++ main.py
@@ -28,6 +28,16 @@ class InitialScreen(Screen):
     def screen_change(self, instance):
         self.manager.current = "interactivescreen"
 
+class TextsFound:
+    def __init__(self, file):
+        self.file = file
+
+    def search_text(self):
+        with open(self.file, 'r') as f:
+            texts = [t.strip() for t in f.readlines() if t.strip()]
+
+        return texts
+
 class InteractiveScreen(Screen):
     def __init__(self, **kwargs):
         super(InteractiveScreen, self).__init__(**kwargs)
@@ -35,39 +45,41 @@ class InteractiveScreen(Screen):
         self.show_text_onScreen()
 
     def show_text_onScreen(self):
-        self.text_font = "O sombrio poder do jovem Malbordus está chegando a seu ápice. Os elfos que o criaram \natribuíram-lhe uma tarefa final: reaver os cinco artefatos do Dragão escondidos por séculos\n na cidade perdida de Vatos, em algum lugar no Deserto dos Crânios. Somente quando os tiver \nem seu poder ele será capaz e rebelar-se e submeter Allansia. Cada dia que passa o aproxima \nmais desse objetivo, e somente VOCÊ pode detê-lo!"
+        self.text_font = TextsFound('historia.txt')
+        self.texts = self.text_font.search_text()
         self.text_label = Label(text='', halign='left')
         self.box_text = BoxLayout(orientation='vertical')
         self.box_text.add_widget(self.text_label)
         self.add_widget(self.box_text)
-        self.words = self.text_font.split()
         self.current_word_index = 0  # índice da palavra atual
-        self.cont_letters = 3
-        Clock.schedule_once(self.add_next_word, 3)  # agendar a adição da primeira palavra após 0.5 segundos
-    
+        self.cont_letters = 0
+        Clock.schedule_once(self.add_next_word, 3)  # agendar a adição da primeira palavra após 3 segundos
+
     def add_next_word(self, dt):
-        if self.current_word_index < len(self.words):
-            word = self.words[self.current_word_index]
+        if self.current_word_index < len(self.texts):
+            word = self.texts[self.current_word_index]
             # Verifica se adicionar a palavra ultrapassa o limite de 50 caracteres por linha
-            if len(self.text_label.text.split('\n')[-1]) + len(word) > 80:
+            if self.cont_letters + len(word) > 50:
                 self.text_label.text += '\n'  # Adiciona uma nova linha
+                self.cont_letters = 0
             self.text_label.text += word + ' '  # Adiciona a palavra
             self.current_word_index += 1
             self.cont_letters += len(word) + 1
-            Clock.schedule_once(self.add_next_word, 0.1)  # Agendar a adição da próxima palavra após 0.5 segundos
-            if self.cont_letters  >= len(self.text_font):
-                Clock.schedule_once(self.add_goodLuck, 2)
-                Clock.schedule_once(self.add_button, 5)
+            Clock.schedule_once(self.add_next_word, 1)  # Agendar a adição da próxima palavra após 0.1 segundos
+        else:
+            Clock.schedule_once(self.add_goodLuck, 2)
+            Clock.schedule_once(self.add_button, 5)
 
-    def add_goodLuck(self,dt):
+    def add_goodLuck(self, dt):
         msg = "Boa Sorte na sua Jornada!"
         self.text_label.text += '\n' * 5
         self.text_label.text += msg
 
-    def add_button(self,dt):
-        button = Button(text="Clique aqui Para iniciar!",size_hint=(None, None), size=(200, 50), pos_hint= {'center_x':0.5,'center_y':0.5})
-        self.box_text.add_widget(button)
-
+    def add_button(self, dt):
+        if not any(isinstance(child, Button) for child in self.box_text.children):
+            button = Button(text="Clique aqui Para iniciar!", size_hint=(None, None), size=(200, 50),
+                            pos_hint={'center_x': 0.5, 'center_y': 0.5})
+            self.box_text.add_widget(button)
 
 class RPGTextApp(App):
     def build(self):
@@ -82,5 +94,4 @@ class RPGTextApp(App):
 if __name__ == '__main__':
     app = RPGTextApp()
     app.run()
-
 
